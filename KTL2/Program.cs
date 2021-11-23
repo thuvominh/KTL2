d47561a148fb30157d4a091d343f429d17c6f94f
thuvominh <thuvtmgcd210164@fpt.edu.vn>
2021-11-23 22:53:48 +0700

    done

---
+++ KTL2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+namespace KTL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //B1
+            //int[] myArr = { 1, 4, 5, 7, 9, 9, 2, 5, 2, 3, 4, 2, 1, 1, 6, 7, 7 };
+            //int sum = 0;
+
+            //for (int i = 0; i < myArr.Length; i++)
+            //{
+            //    sum += myArr[i];
+            //}
+            //Console.WriteLine("Tong cac so trong mang: " + sum);
+
+            //int[] disArr = myArr.Distinct().ToArray();
+
+            //for (int i = 0; i < disArr.Length; i++)
+            //{
+            //    int count = 0;
+            //    for ( int j = 0; j < myArr.Length; j++)
+            //    {
+            //        if (disArr[i] == myArr[j])
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine($"So lan xuat hien so {disArr[1]} la: {count}");
+            //}
+            //int[] oddArr = new int[myArr.Length];
+            //int[] evenArr = new int[myArr.Length];
+
+            //int oddSZ = 0;
+            //int evenSZ = 0;
+
+            //for (int i = 0; i < myArr.Length; i++)
+            //{
+            //    if (myArr[i]%2==0)
+            //    {
+            //        evenArr[evenSZ] = myArr[i];
+            //        evenSZ++;
+            //    }
+            //    else
+            //    {
+            //        oddArr[oddSZ] = myArr[i];
+            //        oddSZ++;
+            //    }
+
+            //}
+            //Console.Write("So le: ");
+            //for ( int i = 0; i < oddSZ; i++)
+            //{
+            //    Console.Write(" " + oddArr[i]);
+            //}
+            //Console.Write("\nSo chan: ");
+            //for (int i = 0; i < evenSZ; i++)
+            //{
+            //    Console.Write(" " + evenArr[i]);
+            //}
+            //Console.WriteLine("\nPhan tu lon nhat: " + myArr.Max());
+            //Console.WriteLine("\nPhan tu be nhat: " + myArr.Min());
+
+            //B2
+
+            //string username;
+            //string Password;
+
+            //Console.Write("Username: ");
+            //var b = Console.ReadLine() == "gcdmember";
+
+            //if (b = true)
+            //{
+                
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Username khong dung: ");
+            //}
+            //Console.Write("Password: ");
+            //var b1 = Console.ReadLine() == "gcd123";
+
+            //if (b1=true)
+            //{
+            //    Console.WriteLine("Dang nhap thanh cong: ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pass sai");
+            //}
+            
+
+            
+                
+
+            
+
+
+        }
+    }
+}
